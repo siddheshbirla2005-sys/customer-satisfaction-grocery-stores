408bf289fba52dd4771adde1589d3543f775b517
siddheshbirla2005-sys <siddheshbirla2005@gmail.com>
2026-09-09 20:23:33 +0530

    Implement binary search in binarsear.c
    This a program of sorted elements and give them a index position

---
+++ binarsear.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+int n, key;
+int low, high, mid;
+int found = 0;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+int a[n];
+printf("Enter sorted elements:\n");
+for(int i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+}
+
+printf("Enter element to search: ");
+scanf("%d", &key);
+low = 0;
+high = n - 1;
+while(low <= high)
+{
+mid = (low + high) / 2;
+if(a[mid] == key)
+{
+printf("Element found at index %d", mid);
+found = 1;
+break;
+}
+else if(key < a[mid])
+{
+high = mid - 1;
+}
+else
+{
+low = mid + 1;
+}
+}
+if(found == 0)
+{
+printf("Element not found");
+}
+return 0;
+}
