3998c7461b73bf98bf15fca208fbed79a6a3990d
siddheshbirla2005-sys <siddheshbirla2005@gmail.com>
2026-09-09 20:28:16 +0530

    Implement bubble sort algorithm in C
    Arranging the Data in the Particular Order

---
+++ bubblesort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+int n, temp;
+
+printf("Enter number of elements: ");
+scanf("%d", &n);
+int a[n];
+printf("Enter elements:\n");
+for(int i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+}
+for(int i = 0; i < n - 1; i++)
+{
+for(int j = 0; j < n - i - 1; j++)
+{
+if(a[j] > a[j + 1])
+{
+temp = a[j];
+a[j] = a[j + 1];
+a[j + 1] = temp;
+}
+}
+}
+printf("Sorted array:\n");
+for(int i = 0; i < n; i++)
+{
+printf("%d ", a[i]);
+}
+return 0;
+}
