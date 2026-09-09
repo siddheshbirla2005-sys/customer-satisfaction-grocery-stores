f1e3cbc9d9b83f82a5e179ab6b1752ee06cb2952
siddheshbirla2005-sys <siddheshbirla2005@gmail.com>
2026-09-09 20:32:53 +0530

    Implement selection sort algorithm in C
    Selects the smallest Element from The unsorted portion and places it at the correct Position

---
+++ selectionsort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+int n, min, temp;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+int a[n];
+printf("Enter elements:\n");
+for(int i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+}
+for(int i = 0; i < n - 1; i++)
+{
+min = i;
+
+for(int j = i + 1; j < n; j++)
+{
+if(a[j] < a[min])
+{
+min = j;
+}
+}
+temp = a[i];
+a[i] = a[min];
+a[min] = temp;
+}
+printf("Sorted array:\n");
+for(int i = 0; i < n; i++)
+{
+printf("%d ", a[i]);
+}
+return 0;
+}
