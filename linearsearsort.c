a874610a334d5faa525ce8713afe1b0aed710d86
siddheshbirla2005-sys <siddheshbirla2005@gmail.com>
2026-09-09 20:12:41 +0530

    Implement linear search algorithm in C

---
+++ linearsearsort.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+int n, key, i, found = 0;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+int a[n];
+printf("Enter elements:\n");
+for(i = 0; i < n; i++)
+{
+scanf("%d", &a[i]);
+}
+printf("Enter element to search: ");
+scanf("%d", &key);
+for(i = 0; i < n; i++)
+{
+if(a[i] == key)
+{
+printf("Element found at index %d", i);
+found = 1;
+break;
+}
+}
+if(found == 0)
+
+{
+printf("Element not found");
+}
+return 0;
+}
